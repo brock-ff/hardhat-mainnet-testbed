2f8195da9754d25c800f85642ead2e9dcff829ee
Brock Smedley <brock@firstfoundry.co>
2021-04-05 12:52:32 -0700

    init project

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+const ETH_URL = process.env.ETH_URL;
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+
+  const blockNumber = await ethers.provider.getBlockNumber();
+  console.log("block", blockNumber);
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.7.3",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      forking: {
+        url: ETH_URL,
+        blockNumber: 12181575
+      }
+    },
+  }
+};
